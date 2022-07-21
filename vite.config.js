69719b8e66696e243643b108ae10c20d7feba664
apisakch <apisakch11@hotmail.com>
2022-07-21 11:47:00 +0700

    prepare deploy2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://OFFTion96.gitlab.io/jsd-jammming-assessment-function/',
+  //base:'https://OFFTion96.gitlab.io/jsd-jammming-assessment-function/',
   plugins: [react()]
 })
